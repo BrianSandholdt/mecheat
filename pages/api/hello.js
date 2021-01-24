acb23bff70f9e6f550250df11a6c23c71b709279
BrianSandholdt <brian@briansandholdt.com>
2021-01-24 20:09:22 +0100

    body is received OK

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,5 +3,6 @@
 export default (req, res) => {
   console.log(req.body);
   res.statusCode = 200;
-  res.json({ name: 'John Doei', inf: JSON.stringify(req.body)});
+//  res.json({ name: 'John Doei', inf: JSON.stringify(req.body)});
+  res.json({ name: 'John Doei', inf: String(req.body)});
 }
