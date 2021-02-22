347f92390e10867db96656f1d2c328172e2bd881
BrianSandholdt <brian@briansandholdt.com>
2021-02-22 20:54:46 +0100

    change defautl creditorNumber

--- pages/api/fi71.js
+++ pages/api/fi71.js
@@ -3,7 +3,7 @@
 export default (req, res) => {
     console.log(req.body);
     const identNumber = Number(12345678901234);
-    const creditorNumber = Number(20745343);
+    const creditorNumber = Number(12345678);
     //const FI71 = "+71<"+identNumber+" +"+creditorNumber+"<";
     const FI71 = `+71<${identNumber} +${creditorNumber}<`;
     res.statusCode = 200;
