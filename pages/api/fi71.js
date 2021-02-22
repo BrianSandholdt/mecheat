fe24f235ee55bf8f747e7bb4950ccc24d54bbaf5
BrianSandholdt <brian@briansandholdt.com>
2021-02-22 20:40:52 +0100

    FI71 test run

---
+++ pages/api/fi71.js
@@ -0,0 +1,10 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default (req, res) => {
+    console.log(req.body);
+    const identNumber = Number(12345678901234);
+    const creditorNumber = Number(20745343);
+    const FI71 = "+71<"+identNumber+" <"+creditorNumber+"<";
+    res.statusCode = 200;
+    res.json({ fi71: String(FI71)});
+  }
