9dd781e74d945f02b0b82008e08e24fd172fff49
BrianSandholdt <brian@briansandholdt.com>
2021-06-16 16:07:53 +0200

    checking if things work

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,5 +4,5 @@ export default (req, res) => {
   console.log(req.body);
   res.statusCode = 200;
 //  res.json({ name: 'John Doei', inf: JSON.stringify(req.body)});
-  res.json({ name: 'John Doei', inf: String(req.body)});
+  res.json({ name: 'John Doei Sand', inf: String(req.body)});
 }
